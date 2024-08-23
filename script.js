95d4251643edec6d8bc75d46c507acf6b6b1326e
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2024-08-23 23:07:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const t = document.getElementById("timer");
 function setTime() {
 		const date = new Date()
-	const currentT = data.currentTime()
-	t.innerText = `${date.getMonth()}/${date.getDate()}/${date.getFullYear()}, `
+	const currentT = formatAMPM(date)
+	t.innerText = `${date.getMonth()}/${date.getDate()}/${date.getFullYear()}, ${currentT}`
 		
 }
-setTime()
+
+
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+	  var seconds = date.getSeconds();
+
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ':' + seconds+ ' ' + ampm;
+  return strTime;
+}
+setInterval(()=>{
+	setTime()
+},1000,)
